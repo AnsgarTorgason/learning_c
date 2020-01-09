d56f50574d754ffed11ddf7421ff724d4016ff2f
Ansgar <57949337+AnsgarTorgason@users.noreply.github.com>
2020-01-09 01:04:07 +0100

    Update week2_date.c

--- week2_date.c
+++ week2_date.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int i,k;
-    scanf("%d %d", &i, &k);
-    printf("%d\n", i+k);
+int main() {
+    int month, day, year;
+
+    //read date in US-format (mm/dd/yyyy)
+    scanf("%02d/%02d/%04d", &month, &day, &year);
+
+    //print it in ISO 8601 format (yyyy-mm-dd)
+    printf("%04d-%02d-%02d", year, month, day);
     return 0;
+
 }
