87704add525164b338557896c1b65654f06d4fe5
Ansgar <57949337+AnsgarTorgason@users.noreply.github.com>
2020-01-09 01:08:44 +0100

    Update week5_calendar.c

--- week5_calendar.c
+++ week5_calendar.c
@@ -5,7 +5,7 @@ int i,days,start;
 
 int main()
 {
-scanf("%d %d",&days,&start);
+scanf("%d %d",&days,&start);  //31, 30 or 28 days  and when is the 1st day(Monday = 1, Tuesday = 2, ...)
 
 for(i = 0 ; i < start ; i++ ){printf("   ");}  // three times empty space
  
